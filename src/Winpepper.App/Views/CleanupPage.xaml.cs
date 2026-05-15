2df6836b78f39622c5977e51c3f0c596190be975
Jesse Vincent <jesse@primeradiant.com>
2026-05-15 23:03:15 +0000

    feat(app): CleanupPage bound to CleanupSettingsViewModel

---
+++ src/Winpepper.App/Views/CleanupPage.xaml.cs
@@ -0,0 +1,55 @@
+#if WINDOWS
+using Microsoft.UI.Text;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Navigation;
+using Winpepper.App.Hosting;
+
+namespace Winpepper.App.Views;
+
+public sealed partial class CleanupPage : Page
+{
+    public CleanupPage() { InitializeComponent(); }
+
+    protected override void OnNavigatedTo(NavigationEventArgs e)
+    {
+        var shell = (AppShell)e.Parameter;
+        var vm = shell.CleanupVm;
+
+        EnabledSwitch.IsOn = vm.Enabled;
+        EnabledSwitch.Toggled += (_, _) => vm.Enabled = EnabledSwitch.IsOn;
+
+        WindowCtxSwitch.IsOn = vm.WindowContextEnabled;
+        WindowCtxSwitch.Toggled += (_, _) => vm.WindowContextEnabled = WindowCtxSwitch.IsOn;
+
+        ProfileCombo.SelectedValue = vm.Profile;
+        ProfileCombo.SelectionChanged += (_, _) =>
+        {
+            if (ProfileCombo.SelectedValue is string s) vm.Profile = s;
+            CustomPromptBox.IsReadOnly = !vm.CustomPromptEditable;
+        };
+        CustomPromptBox.IsReadOnly = !vm.CustomPromptEditable;
+        CustomPromptBox.Document.SetText(TextSetOptions.None, vm.CustomPrompt);
+        CustomPromptBox.TextChanged += (_, _) =>
+        {
+            CustomPromptBox.Document.GetText(TextGetOptions.None, out var text);
+            vm.CustomPrompt = text;
+        };
+
+        MaxTokSlider.Value = vm.MaxNewTokens;
+        MaxTokLabel.Text = $"Max new tokens: {vm.MaxNewTokens}";
+        MaxTokSlider.ValueChanged += (_, _) =>
+        {
+            vm.MaxNewTokens = (int)MaxTokSlider.Value;
+            MaxTokLabel.Text = $"Max new tokens: {vm.MaxNewTokens}";
+        };
+
+        TimeoutSlider.Value = vm.TimeoutMs;
+        TimeoutLabel.Text = $"Timeout: {vm.TimeoutMs} ms";
+        TimeoutSlider.ValueChanged += (_, _) =>
+        {
+            vm.TimeoutMs = (int)TimeoutSlider.Value;
+            TimeoutLabel.Text = $"Timeout: {vm.TimeoutMs} ms";
+        };
+    }
+}
+#endif
